c0bb91c18ea69d3f1e23e9a649d6368699b1f8bb
MehmetAliYildiz <21108730@student.hhs.nl>
2022-04-13 23:44:01 +0200

    Heb de test van cijferTest afgemaakt en een gedeelte van studentTest

--- src/test/com/company/StudentTest.java
+++ src/test/com/company/StudentTest.java
@@ -2,21 +2,12 @@ package com.company;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 import static org.junit.Assert.*;
 
 public class StudentTest {
-
-    @Test
-    public void getAantalExamens() {
-    }
-
-    @Test
-    public void getNaam() {
-    }
-
-    @Test
-    public void getNummer() {
-    }
 
     @Test
     public void studentVerwijderen() {
@@ -28,10 +19,25 @@ public class StudentTest {
 
     @Test
     public void studentenLijst() {
+        ArrayList<Student> alleStudenten = new ArrayList<Student>(
+                Arrays.asList(new Student("PieterJan", 12345678),
+                        (new Student("Mehmet", 12345679)))
+        );
+        assertNotNull(alleStudenten);
+        assertEquals(alleStudenten.size(), 2);
+        assertEquals(alleStudenten.get(0).getNaam(), "PieterJan");
     }
 
     @Test
     public void getStudentByName() {
+        String naam = "Mehmet";
+        boolean correct = false;
+        Student student1 = new Student("Mehmet", 12345678);
+        if (student1.getNaam() == naam) {
+            correct = true;
+        }
+        assertTrue(correct);
+        assertEquals(student1.getNaam(), naam);
     }
 
     @Test
